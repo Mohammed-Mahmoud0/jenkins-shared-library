5fb974c92fd827ce6e33e531601727a0bf75d180
Mohamed Mahmoud <mf.mohamedfawzy0@gmail.com>
2025-11-01 17:36:34 +0200

    fix issue in function

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -1,19 +1,18 @@
 def call(String imageName) {
-    pipeline {
-        agent any
-
-        stages {
-            stage('Build Maven') {
-                steps {
-                    sh "mvn clean install -DskipTests"
-                }
+    node('node1') {
+        stage('Build Maven') {
+            dir("${env.WORKSPACE}") {
+                sh "mvn clean install -DskipTests"
             }
-            stage('Docker Build & Push') {
-                steps {
-                    sh "docker build -t ${imageName}:${env.BUILD_NUMBER} ."
-                    sh "docker login -u mohamedmahmoud00 -p ${env.DOCKERHUB_PASS}"
-                    sh "docker push ${imageName}:${env.BUILD_NUMBER}"
-                }
+        }
+
+        stage('Docker Build & Push') {
+            withCredentials([string(credentialsId: 'dockerhub-pass', variable: 'DOCKERHUB_PASS')]) {
+                sh """
+                docker build -t ${imageName}:${env.BUILD_NUMBER} .
+                echo "$DOCKERHUB_PASS" | docker login -u mohamedmahmoud00 --password-stdin
+                docker push ${imageName}:${env.BUILD_NUMBER}
+                """
             }
         }
     }
