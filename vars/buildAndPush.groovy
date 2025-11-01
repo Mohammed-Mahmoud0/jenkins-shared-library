d2545936629043cb758e9f9a01dc0592d66df940
Mohamed Mahmoud <mf.mohamedfawzy0@gmail.com>
2025-11-01 18:06:19 +0200

    fix fun

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -1,12 +1,14 @@
 def call(String imageName) {
-    node('node1') {
-        stage('Build Maven') {
-            dir("${env.WORKSPACE}") {
-                sh "mvn clean install -DskipTests"
-            }
-        }
 
-        stage('Docker Build & Push') {
+    stage('Build Maven') {
+        dir("${env.WORKSPACE}") {   // make sure we are in the main workspace
+            sh "ls -la"              // debug (optional)
+            sh "mvn clean install -DskipTests"
+        }
+    }
+
+    stage('Docker Build & Push') {
+        dir("${env.WORKSPACE}") {
             withCredentials([string(credentialsId: 'dockerhub-pass', variable: 'DOCKERHUB_PASS')]) {
                 sh """
                 docker build -t ${imageName}:${env.BUILD_NUMBER} .
